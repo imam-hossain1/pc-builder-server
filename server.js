a41529c090d23db243472b486b9a820964a09ce9
imam-hossain <zunayedemon6t9@gmail.com>
2023-08-20 21:30:41 +0600

    correctection vercel.json file

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const { MongoClient, ObjectId } = require('mongodb');
 var cors = require('cors');
 const dotenv = require('dotenv')
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-dotenv.config();
 
 let db;
 
@@ -49,7 +48,6 @@ async function connectToDatabase() {
   }
 
     
-
    catch (err) {
     console.error('Error connecting to MongoDB:', err);
   }
